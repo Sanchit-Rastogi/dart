c5a2335e3b1ed84910581fc92cc9074673e9722f
Sanchit Rastogi <34184127+Sanchit-Rastogi@users.noreply.github.com>
2020-02-07 10:34:58 +0530

    Create main.dart

---
+++ mian.dart
@@ -0,0 +1,35 @@
+// 1. Print
+
+// void main() {
+//   print("Hello world");
+// }
+
+
+// Variable
+  // int - double - boolean - String
+
+// Functions
+  // Void - Returns value - Parameter
+
+// Conditionls
+  // IF - IFElse - nestedIF
+
+// Loops
+  // For - while - DoWhile
+
+// Classes
+  // Classes 
+
+
+// class Area {
+//   int side;
+//   int area(){
+//     return side*side;
+//   }
+// }
+
+// void main(){
+//   var obj = Area();
+//   obj.side = 10;
+//   print(obj.area());
+// }
